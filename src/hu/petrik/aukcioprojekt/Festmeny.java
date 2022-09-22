a489c5e6f8989eb449764d1e4a59b4e510728e5f
Varga Norbert <vnorbi02@gmail.com>
2022-09-22 08:25:19 +0200

    Felépípés

---
+++ src/hu/petrik/aukcioprojekt/Festmeny.java
@@ -0,0 +1,22 @@
+package hu.petrik.aukcioprojekt;
+
+import java.time.LocalDateTime;
+
+public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
+
+    public Festmeny(String cim, String festo, String stilus) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+        this.licitekSzama=0;
+        this.legmagasabbLicit=0;
+        this.elkelt=false;
+    }
+}
